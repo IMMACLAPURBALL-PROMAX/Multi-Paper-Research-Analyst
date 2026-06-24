0dbeb4e37e02b113edfdc380143194dd5eb65944
IMMACLAPURBALL-PROMAX <drainghappier@gmail.com>
2026-06-25 03:53:34 +0400

    Use postinstall hook instead of Webpack to fix Vercel ONNX binary issue

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "onnxruntime-node": false,
-    };
-    return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
+  /* config options here */
 };
 
 export default nextConfig;
