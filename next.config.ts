0215f2aac72e71c6cb493a4e100f01999c042b4d
IMMACLAPURBALL-PROMAX <drainghappier@gmail.com>
2026-06-25 03:57:48 +0400

    Implement Webpack Nullifier to safely bypass native binaries

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "onnxruntime-node": false,
+    };
+    return config;
+  },
+  turbopack: {} // Silences the local Next 16 Turbopack warning
 };
 
 export default nextConfig;
